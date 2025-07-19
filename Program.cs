a37370f4608f3397997c191f6d19c77da9fac918
Crushedice <Crushedice0@gmail.com>
2025-07-19 20:21:50 +0200

    Overall Exception mod

--- Program.cs
+++ Program.cs
@@ -10,31 +10,6 @@ namespace UpdateServer
     internal static class Program
     {
         
-
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            // Get stack trace for the exception with source file information
-            Exception ex = e.ExceptionObject as Exception;
-           // StackTrace st = new StackTrace(e.ExceptionObject as Exception, true);
-          //  StackFrame frame = st.GetFrame(0);
-          //  int line = frame.GetFileLineNumber();
-            
-            
-            SentrySdk.CaptureException(ex, scope =>
-            {
-                StackTrace st = new StackTrace(e.ExceptionObject as Exception, true);
-                StackFrame frame = st.GetFrame(0);
-                int line = frame.GetFileLineNumber();
-                scope.SetExtra("Exception Line", line);
-                scope.SetExtra("Exception Message", ex.Message);
-                scope.SetExtra("Exception StackTrace", ex.StackTrace);
-
-            });
-
-
-
-        }
-
         // [DebuggerStepThrough]
         [STAThread]
         private static void Main(string[] args)
@@ -42,13 +17,9 @@ namespace UpdateServer
             // Initialize Sentry SDK
             SentrySdk.Init(o =>
             {
-                // Tells which project in Sentry to send events to:
+                
                 o.Dsn = "https://f1a2aed8eae385969e335e8f83a5e7f8@logging.rusticaland.ovh/2";
-                //o.Dsn = "http://37fb8cce191246fd8e59806e5fdb1510:3fc984c23fdc4d1bbfe3aa1dfe5ff674@100.83.135.101:9000/3";
-                // When configuring for the first time, to see what the SDK is doing:
                 o.Debug = false;
-                // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-                // We recommend adjusting this value in production.
                 o.TracesSampleRate = 1.0;
                 o.SendClientReports = true;
                 o.AutoSessionTracking = true;
@@ -58,20 +29,37 @@ namespace UpdateServer
                 o.ProfilesSampleRate = 1.0;
                 o.AttachStacktrace = true;
                 o.DiagnosticLevel = SentryLevel.Debug;
-            });
-            SentrySdk.ConfigureScope(s =>
-            {
-                s.AddAttachment("./UpdateServer.pdb");
-            })  ;
+                o.SetBeforeSend((@event, _) =>
+                    {
+                        // Drop an event altogether:
 
-            // Configure WinForms to throw exceptions so Sentry can capture them.
+                        if (@event.Exception != null)
+                        {
+                            var ex = @event.Exception;
+                            StackTrace st = new StackTrace(ex, true);
+                            StackFrame frame = st.GetFrame(0);
+                            int line = frame.GetFileLineNumber();
+                            SentrySdk.ConfigureScope(s =>
+                            {
+                                s.SetExtra("Exception Type", ex.GetType().Name);
+                                s.SetExtra("Exception Message", ex.Message);
+                                s.SetExtra("Exception StackTrace", ex.StackTrace);
+                            });
+
+
+                        }
+
+                        return @event;
+                    }
+                );
+            });
+           
+
             Application.SetUnhandledExceptionMode(UnhandledExceptionMode.ThrowException);
-            //SentrySdk.CauseCrash(CrashType.Managed); 
-            // Add breadcrumb to track application start
             SentrySdk.AddBreadcrumb("Application started", "app.start");
             SentrySdk.CaptureMessage("Application started", SentryLevel.Info);
 
-            new Heart();
+            _=new Heart();
             while (true)
             {
 
